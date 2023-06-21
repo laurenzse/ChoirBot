1796486137cf0c1f8c5c3cce01910de808ee64cd
Laurenz Seidel <LaurenzSeidel@gmail.com>
2023-06-21 16:01:04 +0200

    remove unused command

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ from telegram.ext import (Updater)
 from telegram.utils.request import Request
 
 from src.conversations import absence_conv, wish_song_conv, confused_comment, remind_conv, admin_conv, mechanical_turk, \
-    help_conv, gig_conv, professor_comment, welcome_goodbye_comment, start_conv
+    help_conv, gig_conv, welcome_goodbye_comment, start_conv
 from src.jobs import pre_rehearsal_update, post_rehearsal_update, nonsense_update, save_job
 from src.state import choir_status
 from src.state.bot_status import singer_watcher, all_members_watcher, all_messages_watcher, logger, \
@@ -55,7 +55,6 @@ def main():
     thank_you_comment.add_handlers(dp)
     mechanical_turk.add_handlers(dp)
     welcome_goodbye_comment.add_handlers(dp)
-    professor_comment.add_handlers(dp)
 
     utils.message_jobs.job_queue = updater.job_queue
     utils.message_jobs.add_job('PRE_REHEARSAL_UPDATE',
